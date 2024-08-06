1f7597fae567d906015cf4f77e25e93ef7775911
ankitha.s <ankitha.s@sclera.com>
2024-08-06 15:23:32 +0530

    new1 commit

--- src/main/java/TestTry/TestTry/TestService.java
+++ src/main/java/TestTry/TestTry/TestService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestService {
     public String getMessage() {
-        return "Hello, Spring!";
+        return "Hello,the Spring!";
     }
 }
