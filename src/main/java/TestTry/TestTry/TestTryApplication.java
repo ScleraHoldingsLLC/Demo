2de8e309e8f72e0601f851eccb8cad3a4be7131e
ankitha.s <ankitha.s@sclera.com>
2024-08-08 09:25:47 +0530

    test commit

--- src/main/java/TestTry/TestTry/TestTryApplication.java
+++ src/main/java/TestTry/TestTry/TestTryApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestTryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestTryApplication.class, args);
+
+		SpringApplication.run(TestTryApplication.class, args)
 	}
 
 }
