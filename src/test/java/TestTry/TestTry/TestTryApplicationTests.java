22090563a5a406e3a220b69238aaea095b95cdc4
ankitha.s <ankitha.s@sclera.com>
2024-08-09 09:38:21 +0530

    test1 commit

--- src/test/java/TestTry/TestTry/TestTryApplicationTests.java
+++ src/test/java/TestTry/TestTry/TestTryApplicationTests.java
@@ -3,8 +3,7 @@ package TestTry.TestTry;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,7 +15,7 @@ class TestTryApplicationTests {
 	private TestService testService;
 
 	@Test
-	public void testGetMessage() {
+	 void testGetMessage() {
 		String expectedMessage = "Hello,world!";
 		String actualMessage = testService.getMessage();
 		assertEquals(expectedMessage, actualMessage);
